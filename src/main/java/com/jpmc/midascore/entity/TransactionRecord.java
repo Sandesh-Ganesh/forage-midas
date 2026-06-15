2a63be7fb1807a9a2a7d3a7b82d86e1379079cd0
shreyaprabhu11 <shreyaprabhu119@gmail.com>
2026-06-15 19:16:38 +0530

    Completed Task 4

--- src/main/java/com/jpmc/midascore/entity/TransactionRecord.java
+++ src/main/java/com/jpmc/midascore/entity/TransactionRecord.java
@@ -16,34 +16,40 @@ public class TransactionRecord {
     @Column(nullable = false)
     private float amount;
 
+    @Column
+    private float incentive;
+
     protected TransactionRecord() {
     }
 
-    public TransactionRecord(UserRecord sender, UserRecord reciever, float amount) {
+    public TransactionRecord(UserRecord sender, UserRecord reciever, float amount, float incentive) {
         this.sender = sender;
         this.reciever = reciever;
         this.amount = amount;
+        this.incentive = incentive;
     }
 
     @Override
     public String toString() {
-        return String.format("Transaction[id=%d, sender='%s', recieverId='%s', amount='%f'", id, sender,reciever, amount);
+        return String.format("Transaction Record [id=%d, sender='%s', recieverId='%s', amount='%f', incentive='%f' ", id, sender,reciever, amount, incentive);
     }
 
     public Long getId() {
         return id;
     }
 
-    public UserRecord getSenderId() {
+    public UserRecord getSender() {
         return sender;
     }
 
-    public UserRecord getRecieverId() {
+    public UserRecord getReciever() {
         return reciever;
     }
 
     public void setAmount(float amount) {
         this.amount = amount;
     }
+
+    public void setIncentive(float amount){ this.incentive = amount; }
 
 }
